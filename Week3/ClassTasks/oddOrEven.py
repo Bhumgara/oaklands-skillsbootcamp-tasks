c5a60c4b7ba002ffd78da7916b009bda4a4eb185
Zachary Bhumgara <zac.bon.bon@gmail.com>
2026-02-22 16:30:02 +0000

    add whitespace

--- Week3/ClassTasks/oddOrEven.py
+++ Week3/ClassTasks/oddOrEven.py
@@ -1,4 +1,5 @@
 userInput = input("Input a number: ")
+
 if(not userInput.isnumeric()):
     if(userInput.__contains__("-")):
         if(not userInput.replace("-", "", 1).isnumeric()):
@@ -7,7 +8,9 @@ if(not userInput.isnumeric()):
     else:
         print("Please input a number.")
         exit()
+
 userInput = int(userInput)
+
 if(userInput%2 == 0):
     print(f"{userInput} is an even number!")
 else:
