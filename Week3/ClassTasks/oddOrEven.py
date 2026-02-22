b6caffddf936a993b7b1fb2d888ac0ef1e3cfb52
Zachary Bhumgara <zac.bon.bon@gmail.com>
2026-02-22 16:16:47 +0000

    Add class tasks for odd or even and leap year

---
+++ Week3/ClassTasks/oddOrEven.py
@@ -0,0 +1,14 @@
+userInput = input("Input a number: ")
+if(not userInput.isnumeric()):
+    if(userInput.__contains__("-")):
+        if(not userInput.replace("-", "", 1).isnumeric()):
+            print("Please input a number.")
+            exit()
+    else:
+        print("Please input a number.")
+        exit()
+userInput = int(userInput)
+if(userInput%2 == 0):
+    print(f"{userInput} is an even number!")
+else:
+    print(f"{userInput} is an odd number!")
